e47fb55af133a1cd54b92c5cebd10648c75a8113
yesbaiwan <yesbaiwan@gmail.com>
2026-09-21 01:54:07 +0800

    release: bump version to 2.6.1

--- ld-blocker.user.js
+++ ld-blocker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Linux.do Keyword Blocker
 // @namespace    https://linux.do/
-// @version      2.6.0
+// @version      2.6.1
 // @description  用「类别/标签/标题」规则屏蔽 linux.do 上不想看到的帖子（需登录使用）
 // @author       ℬ𝒶𝒾𝒲𝒶𝓃
 // @match        https://linux.do/*
